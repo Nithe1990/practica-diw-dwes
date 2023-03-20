a423247c1bdd4f07314c2c53ee153c39309c86a7
Nithe1991 <dardomontalvorodrguez@outlook.com>
2023-03-20 21:48:08 +0100

    dao clase

--- API/dao/ClaseDAO.php
+++ API/dao/ClaseDAO.php
@@ -3,24 +3,47 @@
     require_once "./factory.php";
 
     class ClaseDAO extends Factory implements DAO{
-        public function findAll(){
-            $sql = "select * from clase"
+        public static function findAll(){
+            $sql = "select * from clase;";
+            $datos = array();
+            $resultado = parent::ejecutar($sql, $datos);
+            $arrayClases = $resultado->fetchAll(PDO::FETCH_ASSOC);
+            return $arrayClases;
         }
 
-        public function findById($id){
-
+        public static function findById($id){
+            $sql = "select * from clase where id = ?";
+            $datos = array($id);
+            $resultado = parent::ejecutar($sql, $datos);
+            $clase = $resultado->fetch(PDO::FETCH_ASSOC);
+            if($clase) return $clase;
+            return null;
         }
 
-        public function insert(){
-
+        public static function insert($objeto){
+            $sql = "insert into clase values (?,?,?,?,?,?,?,?)";
+            $objeto = (array)$objeto;
+            $datos = array();
+            foreach ($objeto as $atributo) array_push($datos,$atributo);
+            $resultado = parent::ejecutar($sql, $datos);
+            if($resultado->rowCount() == 0) return false;
+            return true;
         }
 
-        public function update(){
-
+        public static function update($objeto){
+            $sql = "update clase set activo = ?, sala = ?, plazas = ?, plazas_ocupadas = ?, fecha_inicio = ?, fecha_fin = ?, idTipo = ?, idMonitor = ? where id = ?";
+            $datos = array($objeto->activo, $objeto->sala, $objeto->plazas, $objeto->plazas_ocupadas, $objeto->fecha_inicio, $objeto->fecha_fin, $objeto->idTipo, $objeto->idMonitor, $objeto->id);
+            $resultado = parent::ejecutar($sql, $datos);
+            if($resultado->rowCount() == 0) return false;
+            return true;
         }
 
-        public function delete(){
-
+        public static function delete($id){
+            $sql = "delete from clase where id = ?";
+            $datos = array($id);
+            $resultado = parent::ejecutar($sql, $datos);
+            if($resultado->rowCount() == 0) return false;
+            return true;
         }
     }
 ?>
