37c7ea6021ee9b3a4231e518ff5148278ac2abf7
Nithe1990 <dardomontalvorodrguez@outlook.com>
2023-03-21 11:02:32 +0100

    controlador clase

--- API/controlador/ClaseControlador.php
+++ API/controlador/ClaseControlador.php
@@ -25,8 +25,31 @@
 
         public function consultar(){
             $parametros = $this->parametros();
-            $recurso = self::recurso();
-            $lista = ClaseDAO
+            $recurso = self::comprobarRecurso();
+            if(count($recurso) == 2){
+                if(!$parametros){
+                    $lista = ClaseDAO::findAll();
+                    $data = json_encode($lista);
+                    self::respuesta($data, array('Content-Type: application/json', 'HTTP/1.1 200 OK'));
+                }else{
+                    self::respuesta('', array('HTTP/1.1 400 No se ha utilizado un filtro correcto'));
+                }
+            }
+
+            if(count($recurso) == 3){
+                $clase = ClaseDAO::findById($recurso[2]);
+                $data = json_encode($clase);
+                self::respuesta($data, array('Content-Type: application/json', 'HTTP/1.1 200 OK'));
+            }
+        }
+
+        public function crear(){
+            $dato = json_decode(file_get_contents('php://input'), true);
+            if(isset($dato['activo']) && isset($dato['sala']) && isset($dato['plazas']) && isset($dato['plazas_ocupadas']) 
+            && isset($dato['fecha_inicio']) && isset($dato['fecha_fin']) && isset($dato['idTipo']) && 
+            isset($dato['idMonitor'])){
+                $clase = new Clase($dato['sala'], $dato['plazas'], $dato['fecha_inicio'], $dato['fecha_fin'], $dato['idTipo'], $dato['idMonitor'])
+            }
         }
     }
 ?>
