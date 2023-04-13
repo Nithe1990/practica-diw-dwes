b27f786693d9f6c08309a7ef3d50c0788686c74a
Nithe1990 <dardomontalvorodrguez@outlook.com>
2023-04-13 13:14:35 +0200

    empezando a ñadir funcionalidades del login

---
+++ WEB/index.php
@@ -0,0 +1,3 @@
+<?
+    include 'vista/layout.php';
+?>
---
+++ WEB/vista/login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>    
+    <main class="container mb-5">
+        <form method="POST" action="index.php" class="mt-5 p-2 border border-dark rounded">
+            <section class="form-floating mb-3">
+                <input type="text" class="form-control" id="txtlogin" placeholder="usuario@mail.com" name="user">
+                <label for="txtlogin">Usuario</label>
+            </section>
+            <section class="form-floating mb-3">
+                <input type="password" class="form-control" id="txtContrasena" placeholder="0000" name="contrasena">
+                <label for="txtContrasena">Contraseña</label>
+            </section>
+            <div class="row d-flex justify-content-evenly mb-3">
+                <div class="col text-center">
+                    <input type="checkbox" name="recordar" id="cbRecordar" value="recordar">
+                    <label for="cbRecordar">Recuérdame</label>
+                </div>
+                <div class="col text-center">
+                    <input type="submit" value="Acceder" name="acceder" class="btn btn-primary">
+                </div>
+                <div class="text-center">
+                    <a href="">Crear cuenta</a>
+                </div>
+            </div>
+        </form>
+    </main>
+</body>
+</html>
