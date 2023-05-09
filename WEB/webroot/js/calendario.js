30d8166e4532973a7c117dba8b6fc8cc29f5136e
Nithe1990 <dardomontalvorodrguez@outlook.com>
2023-05-10 01:02:15 +0200

    avance en calendario

--- WEB/webroot/js/calendario.js
+++ WEB/webroot/js/calendario.js
@@ -1,11 +1,11 @@
-const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-const esBisiesto = (ano) => {
-    return (ano % 400 === 0) ? true :
-    (ano % 100 === 0) ? false :
-    ano % 4 === 0;
-};
-$(document).ready(function(){
-    // pone el mes y año actuales
-    $("span").text(meses[new Date().getMonth()] + " " + new Date().getFullYear());
+import('./bootstrap-datepicker.js');
 
+$(document).ready(function(){
+    $('.datepicker').datepicker({
+        format: "dd/mm/yyyy"
+        // weekStart: 1,
+        // language: "es",
+        // keyboardNavigation: false,
+        // daysOfWeekDisabled: "0,6"
+    });
 });
